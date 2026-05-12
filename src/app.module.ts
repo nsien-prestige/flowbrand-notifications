aa047e9ecd8a0a69c90a72ebf74e2c05fd3c3084
Prestige Nsien <prestigensien45@gmail.com>
2026-05-12 16:40:28 +0100

    chore: setup TypeORM with Supabase PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsModule } from './notifications/notifications.module';
+import { Notification } from './notifications/entities/notification.entity';
+import { NotificationPreference } from './notifications/entities/notification-preference.entity';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        entities: [Notification, NotificationPreference, User],
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
+    }),
+    NotificationsModule,
+  ],
 })
 export class AppModule {}
